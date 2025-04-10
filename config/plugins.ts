37f8635875159f5496f09fefc31ddf6c1f072bc7
Adrian Palcic <adrian.palcic@gmail.com>
2025-04-10 09:08:44 +0200

    new

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,10 @@
-module.exports = {
+module.exports = ({ env }) => ({
     ckeditor: {
         enabled: true,
     },
-};
+    'users-permissions': {
+        config: {
+            jwtSecret: env('JWT_SECRET'),
+        },
+    },
+});
